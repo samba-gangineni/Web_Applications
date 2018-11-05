183d36614368ab10e96dbf809f17799d3d8a6fad
samba693 <sambasivarao693@gmail.com>
2018-11-05 17:01:33 -0500

    App modifications

--- terminal_blog/app.py
+++ terminal_blog/app.py
@@ -1,17 +1,8 @@
 from database import Database
-from models.blog import Blog
-
+from menu import Menu
 __author__ = "Sambasiva Rao Gangineni"
 
 Database.initialize()
 
-blog = Blog(author="Samba",
-            title="Sample title",
-            description="Sample description")
-
-blog.new_post()
-blog.save_to_mongo()
-
-from_database = Blog.from_mongo(blog.id)
-
-print(blog.get_posts())
+menu = Menu()
+menu.run_menu()
