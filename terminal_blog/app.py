00b75c15086a66847e5005d33400f3e241d0202d
samba693 <sambasivarao693@gmail.com>
2018-11-05 16:37:23 -0500

    Getting working of all blog methods

--- terminal_blog/app.py
+++ terminal_blog/app.py
@@ -1,9 +1,17 @@
 from database import Database
-from models.post import Post
+from models.blog import Blog
 
 __author__ = "Sambasiva Rao Gangineni"
 
 Database.initialize()
 
-post = Post("Post1 title", "Post1 Content", "Post1 author")
-post2 = Post("Post2 title", "Post2 Content", "Post2 author")
+blog = Blog(author="Samba",
+            title="Sample title",
+            description="Sample description")
+
+blog.new_post()
+blog.save_to_mongo()
+
+from_database = Blog.from_mongo(blog.id)
+
+print(blog.get_posts())
--- terminal_blog/models/post.py
+++ terminal_blog/models/post.py
@@ -1,12 +1,13 @@
 import uuid
 from database import Database
+from datetime import datetime as dt
 
 __author__ = 'Sambasiva Rao Gangineni'
 
 class Post(object):
     
     #Properties each post have
-    def __init__(self,blog_id, title,content,author, date = today, id=None):
+    def __init__(self,blog_id, title,content,author, date = dt.utcnow(), id=None):
         self.blog_id = blog_id
         self.title = title
         self.content = content
@@ -29,10 +30,15 @@ class Post(object):
             'created_date' : self.created_date
         }
 
-    @staticmethod
-    def from_mongo(id):
-        return Database.find_one(collection='posts',query={'id':id})
-
+    @classmethod
+    def from_mongo(cls , id):
+        post_data = Database.find_one(collection='posts',query={'id':id})
+        return cls(blog_id = post_data['blog_id'], 
+                    title = post_data['title'],
+                    content=post_data['content'],
+                    author=post_data['author'], 
+                    date = post_data['date'], 
+                    id=post_data['id'])
     @staticmethod
     def from_blog(id):
-        return [post for post in Database.find(collection='posts',query={'id':id})]
+        return [post for post in Database.find(collection='posts',query={'blog_id':id})]
