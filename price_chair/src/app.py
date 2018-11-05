287601aee07b2548335613a76beef510821c4b7b
samba693 <sambasivarao693@gmail.com>
2018-11-05 12:55:56 -0500

    Comparing prices of chair

---
+++ price_chair/src/app.py
@@ -0,0 +1,10 @@
+__author__ = "Sambasiva Rao Gangineni"
+
+import requests
+from bs4 import BeautifulSoup
+
+request = requests.get("https://www.amazon.com/Children-Office-Swivel-Chairs-Adjust/dp/B07BR3R52X/ref=sr_1_1_sspa?ie=UTF8&qid=1541439343&sr=8-1-spons&keywords=study+chairs&psc=1")
+content = request.content
+soup = BeautifulSoup(content,"html.parser")
+element = soup.find("span",{"id":"priceblock_ourprice","class":"a-size-medium a-color-price"},recursive=True)
+string_price = element.text.strip()
